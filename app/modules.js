48b77538fd94667d42933c2e25aeb389bf39d188
KanKK <286454796@qq.com>
2017-06-25 00:23:24 +0800

    modules and flowControl

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,14 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
-  createModule: function(str1, str2) {
+  createModule: function (str1, str2) {
+    return {
+      greeting: str1,
+      name: str2,
 
+      sayIt() {
+        return this.greeting + ', ' + this.name;
+      }
+    }
   }
 };
