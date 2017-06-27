2416a493a6a2b1a33f42311cc0218b4aa73dc66c
kankk <286454796@qq.com>
2017-06-27 15:44:24 +0800

    objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    const iterateArr = [];
+    for(let key of Object.getOwnPropertyNames(obj)) {
+      iterateArr.push(`${key}: ${obj[key]}`); 
+    }
+    return iterateArr;
   }
 };
