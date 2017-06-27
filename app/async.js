9885ce8107fc35c9c7b1b2a0496d1c4818c00b67
kankk <286454796@qq.com>
2017-06-27 12:14:33 +0800

    async

--- app/async.js
+++ app/async.js
@@ -1,11 +1,20 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
-
+  async: function (value) {
+    return new Promise((resolve, reject) => {
+      resolve(value);
+    });
   },
 
-  manipulateRemoteData: function(url) {
-
+  manipulateRemoteData: function (url) {
+    return new Promise((resolve, reject) => {
+      const json = require('..' + url);
+      const result = [];
+      for (let people of json.people) {
+        result.push(people.name);
+      }
+      resolve(result.sort());
+    });
   }
 };
