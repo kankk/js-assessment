a80f87d5aa1df49eec15552a42d1a4e5c2345dfd
kankk <286454796@qq.com>
2017-06-28 18:33:45 +0800

    strings

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function (str, amount) {
-
+    const strInfo = {
+      lastChar: '',
+      count: 0,
+    };
+    const strArr = [];
+    for (let c of str) {
+      if (c !== strInfo.lastChar) {
+        strArr.push(c);
+        strInfo.lastChar = c;
+        strInfo.count = 0;
+        strInfo.count++;
+      } else {
+        if (strInfo.count < amount) {
+          strArr.push(c);
+        }
+        strInfo.lastChar = c;
+        strInfo.count++;
+      }
+    }
+    return strArr.join("");
   },
 
   wordWrap: function (str, cols) {
