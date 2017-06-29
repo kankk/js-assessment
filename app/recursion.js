771c130c720c6500454e132024e156ef2a5a79a0
kankk <286454796@qq.com>
2017-06-29 17:14:47 +0800

    recursion

--- app/recursion.js
+++ app/recursion.js
@@ -1,19 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
+  listFiles: function (data, dirName) {
 
   },
 
-  permute: function(arr) {
+  permute: function (arr) {
 
   },
 
-  fibonacci: function(n) {
-
+  fibonacci: function (n) {
+    if (n <= 2) {
+      return 1;
+    } else {
+      return this.fibonacci(n - 1) + this.fibonacci(n - 2);
+    }
   },
 
-  validParentheses: function(n) {
+  validParentheses: function (n) {
 
   }
 };
